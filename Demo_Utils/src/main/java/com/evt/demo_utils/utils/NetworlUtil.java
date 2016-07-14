062c5400e0807c456cc017cd1e0721f500cdbca4
gmrxus <gmrxus@126.com>
2016-07-14 16:39:57 +0800

    utils添加权限

--- Demo_Utils/src/main/java/com/evt/demo_utils/utils/NetworlUtil.java
+++ Demo_Utils/src/main/java/com/evt/demo_utils/utils/NetworlUtil.java
@@ -8,6 +8,9 @@ import android.net.NetworkInfo;
  * Created by mrxus on 16/7/14.
  */
 public class NetworlUtil {
+    //需要添加权限:<uses-permission android:name="android.permission.ACCESS_NETWORK_STATE"/>
+
+
     public static boolean isConnect(Context context) {
         if (context != null) {
             ConnectivityManager mConnectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
