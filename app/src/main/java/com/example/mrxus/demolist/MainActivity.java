24d41058c56c3a4fd83c0836398ebe7c302290fb
gmrxus <gmrxus@126.com>
2016-07-18 01:44:45 +0800

    软键盘遮挡布局问题

--- app/src/main/java/com/example/mrxus/demolist/MainActivity.java
+++ app/src/main/java/com/example/mrxus/demolist/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.mrxus.demolist;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.orhanobut.logger.Logger;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private CountDownTimer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        init();
+    }
+
+    private void init() {
+        findViewById(R.id.bt).setOnClickListener(this);
+        findViewById(R.id.bt1).setOnClickListener(this);
+
+
+        timer = new CountDownTimer(10000,1000) {
+            @Override
+            public void onTick(long l) {
+                Logger.e("onTick"+l/1000);
+            }
+
+            @Override
+            public void onFinish() {
+                Logger.e("onFinish");
+            }
+
+        };
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bt:
+                timer.start();
+                break;
+            case R.id.bt1:
+                Toast.makeText(this, "bt1", Toast.LENGTH_SHORT).show();
+                timer.cancel();
+                break;
+        }
+
     }
 }
---
+++ demo_soft_input/src/main/java/com/evt/demo_soft_input/MainActivity.java
@@ -0,0 +1,22 @@
+package com.evt.demo_soft_input;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void jump(View view) {
+        startActivity(new Intent(this,SecondActivity.class));
+    }
+}
