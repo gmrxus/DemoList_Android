c596a99337c585e39cce45ad8902b75ec66dadae
gmrxus <gmrxus@126.com>
2016-07-29 15:57:01 +0800

    简化zxing

--- app/src/main/java/com/example/mrxus/demolist/MainActivity.java
+++ app/src/main/java/com/example/mrxus/demolist/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.mrxus.demolist;
 
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        findViewById(R.id.bt).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    private class DataPickerFragment extends DialogFragment{
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return super.onCreateDialog(savedInstanceState);
+
+
+        }
     }
 }
